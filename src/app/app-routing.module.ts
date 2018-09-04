a1648891a4e70e3d839f54ac76ada657f068de23
Robert97 <hakobyan.robert97@gmail.com>
2018-09-04 15:19:53 +0400

    Upd

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 // route components
 import { LandingComponent } from './landing/landing.component';
+import { MainComponent } from './main/main.component';
+import { ProfileComponent } from './profile/profile.component';
+
 
 const routes: Routes = [
   { path: '', component: LandingComponent },
+  { path: 'main', component: MainComponent },
+  { path: 'profile', component: ProfileComponent },
   { path: '**', redirectTo: '/', pathMatch: 'full' }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,10 @@ import { HttpModule } from '@angular/http';
 // components
 import { AppComponent } from './app.component';
 import { LandingComponent } from './landing/landing.component';
+import { MainComponent } from './main/main.component';
+import { ProfileComponent } from './profile/profile.component';
+import { LoadingComponent } from './loading/loading.component';
+import { HeaderComponent } from './header/header.component';
 
 // services
 import { HttpService } from './http.service';
@@ -16,7 +20,11 @@ import { HttpService } from './http.service';
 @NgModule({
   declarations: [
     AppComponent,
-    LandingComponent
+    LandingComponent,
+    MainComponent,
+    ProfileComponent,
+    LoadingComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
