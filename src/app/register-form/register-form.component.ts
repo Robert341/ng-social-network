fb4fd6430ccc85937f2c9cc2297928532641eab5
Robert97 <hakobyan.robert97@gmail.com>
2018-09-06 12:32:34 +0400

    Upd

--- src/app/register-form/register-form.component.ts
+++ src/app/register-form/register-form.component.ts
@@ -10,6 +10,14 @@ import { HttpService } from '../http.service';
 export class RegisterFormComponent implements OnInit {
 
   registerForm: FormGroup;
+  formChecked = false;
+  inFocus = {
+    firstName: false,
+    lastName: false,
+    email: false,
+    pass: false,
+    rePass: false
+  };
 
   constructor(
     private _formBuilder: FormBuilder,
@@ -22,18 +30,22 @@ export class RegisterFormComponent implements OnInit {
 
   createRegisterForm() {
     this.registerForm = this._formBuilder.group({
-      firstName: [null, Validators.required],
-      lastName: [null, Validators.required],
+      firstName: [null, [Validators.required, Validators.pattern(/^[A-Z][a-z]+$/)]],
+      lastName: [null, [Validators.required, Validators.pattern(/^[A-Z][a-z]+$/)]],
       dateOfBirth: [null, Validators.required],
       gender: [null, Validators.required],
-      email: [null, Validators.required],
-      pass: [null, Validators.required],
+      email: [null, [Validators.required, Validators.pattern(/^[a-zA-Z0-9_.-]+@+[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]{2,4}$/)]],
+      pass: [null, [Validators.required, Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,12}$/)]],
       rePass: [null, Validators.required]
     });
   }
 
   onRegister() {
-    console.log(this.registerForm.value);
+    this.formChecked = true;
+
+    if (this.registerForm.valid && this.registerForm.value.pass === this.registerForm.value.rePass) {
+      console.log(this.registerForm.value);
+    }
     /*
     this._userService.register(this.registerForm.value).subscribe(resRegisterAttempt => {
       if (resRegisterAttempt.success === true) {
