5f8806dca261ed6e1100fbb610ea93760cd67e71
Robert97 <hakobyan.robert97@gmail.com>
2018-09-10 18:53:02 +0400

    Upd

--- src/app/post-section/post-section.component.ts
+++ src/app/post-section/post-section.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { HttpService } from '../http.service';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-post-section',
@@ -7,15 +10,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostSectionComponent implements OnInit {
 
+  publishForm: FormGroup;
   inFocus = false;
 
-  constructor() { }
+  constructor(private _formBuilder: FormBuilder, private _httpService: HttpService) { }
 
   ngOnInit() {
+    this.createPublishForm();
+  }
+
+
+  createPublishForm() {
+    this.publishForm = this._formBuilder.group({
+      message: [],
+      images: [[]],
+      videos: [[]],
+      audios: [[]]
+    });
+  }
+
+  activateInput(id) {
+    $('#' + id).click();
+  }
+
+  changeAttachedFiles(filesName, e) {
+    this.publishForm.value[filesName] = this.publishForm.value[filesName].concat(e.target.files);
+    console.log(this.publishForm.value[filesName]);
   }
 
   publish() {
-
+    console.log(this.publishForm.value);
   }
 
 }
